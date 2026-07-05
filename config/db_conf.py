5e9d1f994e00d648045e7c55216e9889300f0183
zhouming <minerchow@126.com>
2026-07-05 10:38:22 +0800

    update

--- config/db_conf.py
+++ config/db_conf.py
@@ -40,7 +40,9 @@ async_engine = create_async_engine(
     ASYNC_DATABASE_URL,
     echo=(ENV == "development"),
     pool_size=10,
-    max_overflow=20
+    max_overflow=20,
+    pool_pre_ping=True,
+    pool_recycle=3600
 )
 
 AsyncSessionLocal = async_sessionmaker(
