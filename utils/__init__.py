2a767377a914d0c6573a259e01334576888bdfc6
zhouming <minerchow@126.com>
2026-07-12 10:13:05 +0800

    update

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,6 +2,7 @@ from .response import success_response, error_response
 from .security import get_hash_password, verify_password
 from .exception_handlers import register_exception_handlers
 from .auth import get_current_user
+from .permissions import allow_user, allow_author, allow_admin
 
 __all__ = [
     "success_response",
@@ -9,5 +10,8 @@ __all__ = [
     "get_hash_password",
     "verify_password",
     "register_exception_handlers",
-    "get_current_user"
+    "get_current_user",
+    "allow_user",
+    "allow_author",
+    "allow_admin",
 ]
